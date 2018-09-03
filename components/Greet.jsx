4fcdf514a09f33d9814b2163924af92bd8357db6
Jdraiv <josuedr11@gmail.com>
2018-09-02 19:44:02 -0600

    Initial commit

---
+++ components/Greet.jsx
@@ -0,0 +1,11 @@
+
+export default class Greeter extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return <h1>Hello {this.props.name}</h1>
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+// Import the component and call it :)
+
+import Greeter from '../components/Greet'
+
+
+export default class App extends React.Component {
+    render() {
+        return <Greeter name="User" />
+    }
+}
